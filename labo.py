c0d998b4f3fca253eacdbc99fa24cf4cabf5b1c2
darius <tokamdarius@gmqil.com>
2025-08-28 12:24:13 -0400

    f4

--- labo.py
+++ labo.py
@@ -20,4 +20,7 @@ elif choix.upper() == "DEUX":
     GPIO.output(4, GPIO.HIGH)
     print(" les Lumieres  sont ouverte")
     GPIO.output(17, GPIO.HIGH)
-
+elif choix.upper() == "TROIS":
+    GPIO.output(4, GPIO.LOW)
+    print(" les Lumieres  sont eteinte")
+    GPIO.output(17, GPIO.LOW)
